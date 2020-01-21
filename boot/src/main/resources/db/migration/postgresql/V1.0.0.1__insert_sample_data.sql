608c90ac85df73256e7ff4ea49e439d8e9a4eff7
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-01-21 15:07:23 +0700

    - add flyway dependency - add migration scripts

---
+++ boot/src/main/resources/db/migration/postgresql/V1.0.0.1__insert_sample_data.sql
@@ -0,0 +1,3 @@
+insert into customer (id, username, role) values (0, 'batman', 'the dark knight');
+insert into customer (id, username, role) values (1, 'joker', 'the clown prince of crime');
+insert into customer (id, username, role) values (2, 'catwoman', 'the thief');
