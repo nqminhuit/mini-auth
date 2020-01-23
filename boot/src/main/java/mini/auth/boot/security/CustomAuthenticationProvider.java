fef105df93c6e13954735816c45cb17fb16845a8
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-01-23 23:10:12 +0700

    - centralize all CustomerRepository instances to MyUserDetailsService and use only this service to get UserDetails

--- boot/src/main/java/mini/auth/boot/security/CustomAuthenticationProvider.java
+++ boot/src/main/java/mini/auth/boot/security/CustomAuthenticationProvider.java
@@ -1,42 +1,33 @@
 package mini.auth.boot.security;
 
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import mini.auth.boot.entities.Customer;
-import mini.auth.boot.entities.repository.CustomerRepository;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
-    // private static final Logger LOG = LogManager.getLogger(CustomAuthenticationProvider.class);
-
     @Autowired
-    private CustomerRepository customerRepository;
+    private MyUserDetailsService userDetailsService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        Customer customer = customerRepository.findByUsername(username);
-        if (customer == null) {
-            throw new UsernameNotFoundException("Username not found!");
-        }
-
-        if (!password.equals(customer.getPassword())) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        String userDetailsPassword = userDetails.getPassword();
+        if (!password.equals(userDetailsPassword)) {
             throw new BadCredentialsException("Password is invalid!");
         }
 
         return new UsernamePasswordAuthenticationToken(
-            username, password, Arrays.asList(new SimpleGrantedAuthority(customer.getRole())));
+            userDetails, userDetailsPassword, userDetails.getAuthorities());
     }
 
     @Override
--- boot/src/main/java/mini/auth/boot/security/filter/JwtRequestFilter.java
+++ boot/src/main/java/mini/auth/boot/security/filter/JwtRequestFilter.java
@@ -1,51 +1,50 @@
 package mini.auth.boot.security.filter;
 
 import java.io.IOException;
-import java.util.Arrays;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import mini.auth.boot.security.JwtService;
+import mini.auth.boot.security.MyUserDetailsService;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    private static final Logger LOG = LogManager.getLogger(JwtRequestFilter.class);
-
     @Autowired
     private JwtService jwtService;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
 
-        String jwt = request.getHeader("Authorization");
+        String authorizationHeader = request.getHeader("Authorization");
+        if (StringUtils.isNotBlank(authorizationHeader)) {
+            String jwt = authorizationHeader.substring("Bearer ".length());
+            if (StringUtils.isNotBlank(jwt)) {
+                String username = jwtService.extractUsername(jwt);
+                String userRole = jwtService.extractUserRole(jwt);
 
-        if (StringUtils.isNotBlank(jwt)) {
-            String username = jwtService.extractUsername(jwt);
-            String userRole = jwtService.extractUserRole(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            UserDetails userDetails = new User(username, "somepassword",
-                Arrays.asList(new SimpleGrantedAuthority(userRole)));
+                UsernamePasswordAuthenticationToken upat =
+                    new UsernamePasswordAuthenticationToken(
+                        userDetails, userDetails.getPassword(), userDetails.getAuthorities());
 
-            UsernamePasswordAuthenticationToken upat =
-                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-
-            upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(upat);
+                upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(upat);
+            }
         }
 
         filterChain.doFilter(request, response);
