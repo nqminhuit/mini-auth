64d263235576a4bda91d9af643993b39d00109bd
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-01-27 15:19:54 +0700

    - re-enable swagger without MVC configuration and resouce handlers

--- boot/src/main/java/mini/auth/boot/SwaggerConfig.java
+++ boot/src/main/java/mini/auth/boot/SwaggerConfig.java
@@ -1,19 +1,19 @@
 package mini.auth.boot;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-// @Configuration
-// @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -27,19 +27,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-        .title("Mini Auth Rest API")
-        .description("\"APIs provided by the Spring boot back-end for front-end component")
-        .version("1.0.0")
-        .build();
-    }
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+            .title("Mini Auth Rest API")
+            .description("\"APIs provided by the Spring boot back-end for front-end component")
+            .version("1.0.0")
+            .build();
     }
 
 }
