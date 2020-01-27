1e839548939a488eb623c8ed8bb30f70de32a5e9
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-01-27 09:56:06 +0700

    - add page not found component - move routes to app-routing module

--- mini-auth-ng/src/app/app-routing.module.ts
+++ mini-auth-ng/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { CustomerTableComponent } from './customer-table/customer-table.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: 'customer', component: CustomerTableComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
