dfd59d13ab520d8c77c5f9e17bcf5881c415d672
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2020-02-03 16:14:40 +0700

    - add interface method on customer repository to find all customer and order by id.

--- mini-auth-persistence/src/main/java/mini/auth/repository/CustomerRepository.java
+++ mini-auth-persistence/src/main/java/mini/auth/repository/CustomerRepository.java
@@ -1,10 +1,13 @@
 package mini.auth.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import mini.auth.entity.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     Customer findByUsername(String username);
+
+    List<Customer> findAllByOrderByIdAsc(); // 2 "By"s is requried
 
 }
